c65c820b7af8647febfdd726d0907089aba1fbf6
Andrey Fedorov <oran9e.red@gmail.com>
2025-11-20 07:32:00 +0100

    Fix zig example

--- src/leibniz.zig
+++ src/leibniz.zig
@@ -1,20 +1,23 @@
 const std = @import("std");
 
-pub fn main() anyerror!void {
+pub fn main() !void {
     var file = try std.fs.cwd().openFile("rounds.txt", .{});
     defer file.close();
     var buffer: [1024]u8 = undefined;
-    var n = try file.read(buffer[0..buffer.len]);
+    const n = try file.read(buffer[0..buffer.len]);
     var rounds = try std.fmt.parseInt(i64, std.mem.trim(u8, buffer[0..n], "\n"), 10);
     rounds += 2;
-
-    var i: usize = 2;
+    var i: i64 = 2;
     var x: f64 = 1.0;
     var pi: f64 = 1.0;
     while (i < rounds) : (i += 1) {
         x = -x;
-        pi += (x / @intToFloat(f64, 2 * i - 1));
+        pi += (x / @as(f64, @floatFromInt(2 * i - 1)));
     }
     pi *= 4;
-    try std.io.getStdOut().writer().print("{}", .{pi});
+
+    var stdout_buffer: [256]u8 = undefined;
+    var stdout_writer = std.fs.File.stdout().writer(&stdout_buffer);
+    try stdout_writer.interface.print("{d}", .{pi});
+    try stdout_writer.interface.flush();
 }
