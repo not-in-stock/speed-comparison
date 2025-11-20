92290e996c4e6aad0368d667d53844f2e8a8a2fb
Andrey Fedorov <oran9e.red@gmail.com>
2025-11-20 04:30:19 +0100

    Optimize Clojure solution

--- src/leibniz.clj
+++ src/leibniz.clj
@@ -3,17 +3,21 @@
 (set! *unchecked-math* :warn-on-boxed)
 
 (defn calc-pi-leibniz
-  "Translation of Java solution to Clojure"
+  "Optimized Clojure solution"
+  ^double
   [^long rounds]
-  (let [end (+ 2 rounds)]
-    (loop [i 2 x 1.0 pi 1.0]
-      (if (= i end)
-        (* 4 pi)
-        (let [x (- x)]
-          (recur (inc i) x (+ pi (/ x (dec (* 2 i))))))))))
+  (let [^long end (+ 2 rounds)]
+    (loop [^long i 2
+           ^double x 1.0
+           ^double pi 1.0]
+      (if (== i end)
+        (* 4.0 pi)
+        (let [^double x-neg (- x)
+              ^double denom (double (unchecked-dec (unchecked-multiply 2 i)))]
+          (recur (unchecked-inc i) x-neg (+ pi (/ x-neg denom))))))))
 
-(defn parse-int [s]
-  (Integer/parseInt (re-find #"\A-?\d+" s)))
+(defn parse-long-trim [s]
+  (Long/parseLong (re-find #"\A-?\d+" (str s))))
 
-(def rounds (parse-int (slurp "rounds.txt")))
+(def rounds (parse-long-trim (slurp "rounds.txt")))
 (println (calc-pi-leibniz rounds))
