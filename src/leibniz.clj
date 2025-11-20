7cd5b116e22a7e0d3310f310497f7deb9048b638
Andrey Fedorov <oran9e.red@gmail.com>
2025-11-20 04:42:30 +0100

    Optimize solution more

--- src/leibniz.clj
+++ src/leibniz.clj
@@ -3,18 +3,21 @@
 (set! *unchecked-math* :warn-on-boxed)
 
 (defn calc-pi-leibniz
-  "Optimized Clojure solution"
   ^double
   [^long rounds]
-  (let [^long end (+ 2 rounds)]
-    (loop [^long i 2
-           ^double x 1.0
-           ^double pi 1.0]
-      (if (== i end)
-        (* 4.0 pi)
-        (let [^double x-neg (- x)
-              ^double denom (double (unchecked-dec (unchecked-multiply 2 i)))]
-          (recur (unchecked-inc i) x-neg (+ pi (/ x-neg denom))))))))
+  (let [pairs (quot rounds 2)]
+    (loop [i (long 0)
+           pi (double 0.0)]
+      (if (== i pairs)
+        (let [pi (if (== (bit-and rounds 1) 1)
+                   (+ pi (/ 1.0 (double (unchecked-add (unchecked-multiply 2 i) 1))))
+                   pi)]
+          (* 4.0 pi))
+        (let [k (unchecked-multiply 4 i)
+              denom1 (double (unchecked-inc k))
+              denom2 (double (unchecked-add k 3))
+              term (- (/ 1.0 denom1) (/ 1.0 denom2))]
+          (recur (unchecked-inc i) (+ pi term)))))))
 
 (defn parse-long-trim [s]
   (Long/parseLong (re-find #"\A-?\d+" (str s))))
